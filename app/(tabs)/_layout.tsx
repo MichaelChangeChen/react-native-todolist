53c6b334bd83fedb15458b6912ee8b2a1491b29a
MichaelChangeChen <michaelexgt@gmail.com>
2025-05-09 11:19:15 +0800

    creat todolist

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,7 +1,7 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 import { Platform } from 'react-native';
-
+import 'react-native-gesture-handler';
 import { HapticTab } from '@/components/HapticTab';
 import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
@@ -26,20 +26,34 @@ export default function TabLayout() {
           default: {},
         }),
       }}>
-      <Tabs.Screen
+      {/* <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
-      />
+      /> */}
       <Tabs.Screen
+        name="todoList"
+        options={{
+          title: 'TodoList',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="doc.text.fill" color={color} />,
+        }}
+      />
+      {/* <Tabs.Screen
+        name="todoList"
+        options={{
+          title: 'TodoList',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="text.fill" color={color} />,
+        }}	
+      /> */}
+      {/* <Tabs.Screen
         name="explore"
         options={{
           title: 'Explore',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
-      />
+      /> */}
     </Tabs>
   );
 }
